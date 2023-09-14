87d586e3dc934d10c10d6bb5e40079415ce3746d
Ash Clark <amclark42@gmail.com>
2023-09-14 11:24:15 -0400

    Start editing JS to copy nav menu to the sidebar on load.
    The Bootstrap v3 method of collapsing menus is interfering with the
    v4 method.

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
-console.log("script loaded");
+/*
+  Javascript for the Word Vector Interface
+ */
 
+let toggleSideNav = function(e) {
+  console.log('Toggling navbar visibility');
+  console.log(e);
+  let sideMenu = document.getElementById("navbarResponsive");
+  if ( sideMenu !== undefined ) {
+    copyNavMenu();
+  } /*else if ( sideMenu.classList.contains('show') ) {
+    
+  } else {
+    
+  }*/
+  sideMenu.classList.toggle('show');
+};
 
+// Duplicate the collapsed menu in the header.
+let copyNavMenu = function() {
+  if ( ! document.getElementById("navbarResponsive") ) {
+    let menuEl = document.createElement('section'),
+        menuList = document.getElementById("navbarResponsiveBase").children[0],
+        sidebar = document.getElementById("sidebarCollapsed");
+    menuEl.setAttribute('id', 'navbarResponsive');
+    menuEl.classList.add('collapse');
+    menuEl.append(menuList.cloneNode(true));
+    menuEl.addEventListener('shown.bs.collapse', function(e) {
+      console.log("SHOW MENU PLZ");
+      menuEl.classList.toggle('show');
+    });
+    sidebar.prepend(menuEl);
+    //$('.collapse').collapse();
+  }
+};
+
+// Create a callback function to be run when the entire document has loaded.
+let onLoad = function() {
+  console.log("Page loaded");
+  copyNavMenu();
+  // When the hamburger menu is clicked, toggle the visibility of the nav menu.
+  //document.getElementById("sidebar-hider").onclick = toggleSideNav;
+};
+
+/* Ensure that the callback function above is run, whether or not the DOM has 
+  already been loaded. Solution by Julian Kühnel: 
+  https://www.sitepoint.com/jquery-document-ready-plain-javascript/ */
+if ( document.readyState === 'complete' 
+   || ( document.readyState !== 'loading' && !document.documentElement.doScroll ) 
+   ) {
+  onLoad();
+} else {
+  document.addEventListener('DOMContentLoaded', onLoad);
+}
+/*
 var t = document.getElementById("sidebarCollapsed");
 t.classList.add('side-open');
 
@@ -21,3 +73,4 @@ elem.onclick = function() {
 
         return false;
     };
+*/
