cd465d171dd652c08ff4beb8b422b6711cfb0bb0
Ash Clark <amclark42@gmail.com>
2023-09-15 10:13:43 -0400

    Clean up JS, tweak CSS

--- script.js
+++ script.js
@@ -2,21 +2,18 @@
   Javascript for the Word Vector Interface
  */
 
+/* Toggle the Bootstrap v4 "show" class on the sidebar menu. Currently unused. 
+  Instead, the v3 "in" class styling is replicated in wvi.css. */
 let toggleSideNav = function(e) {
   console.log('Toggling navbar visibility');
-  console.log(e);
   let sideMenu = document.getElementById("navbarResponsive");
   if ( sideMenu !== undefined ) {
     copyNavMenu();
-  } /*else if ( sideMenu.classList.contains('show') ) {
-    
-  } else {
-    
-  }*/
+  }
   sideMenu.classList.toggle('show');
 };
 
-// Duplicate the collapsed menu in the header.
+/* Duplicate the collapsed menu in the header. */
 let copyNavMenu = function() {
   if ( ! document.getElementById("navbarResponsive") ) {
     let menuEl = document.createElement('section'),
@@ -25,16 +22,11 @@ let copyNavMenu = function() {
     menuEl.setAttribute('id', 'navbarResponsive');
     menuEl.classList.add('collapse');
     menuEl.append(menuList.cloneNode(true));
-    menuEl.addEventListener('shown.bs.collapse', function(e) {
-      console.log("SHOW MENU PLZ");
-      menuEl.classList.toggle('show');
-    });
     sidebar.prepend(menuEl);
-    //$('.collapse').collapse();
   }
 };
 
-// Create a callback function to be run when the entire document has loaded.
+/* Create a callback function to be run when the entire document has loaded. */
 let onLoad = function() {
   console.log("Page loaded");
   copyNavMenu();
@@ -52,25 +44,3 @@ if ( document.readyState === 'complete'
 } else {
   document.addEventListener('DOMContentLoaded', onLoad);
 }
-/*
-var t = document.getElementById("sidebarCollapsed");
-t.classList.add('side-open');
-
-
-var elem = document.getElementById("sidebar-hidder");
-elem.onclick = function() {
-        var t = document.getElementById("sidebarCollapsed");
-
-        if (t.classList.contains('side-open')) {
-          // The box that we clicked has a class of bad so let's remove it and add the good class
-          t.classList.remove('side-open');
-          t.classList.add('side-close');
-        } else {
-          t.classList.add('side-open');
-          t.classList.remove('side-close');
-          console.log("You can proceed!");
-        }
-
-        return false;
-    };
-*/
